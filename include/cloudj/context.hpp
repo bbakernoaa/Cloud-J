cf3659100df3e6468df9a334db3ea2ab3f5c6861
bbakernoaa <barry.baker@noaa.gov>
2026-07-29 09:55:15 -0400

    feat: define core model namespaces and context layout

---
+++ include/cloudj/context.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace CloudJ {
+
+struct Context {
+    static constexpr int max_layers = 80;
+    static constexpr int num_wavelengths = 18;
+    static constexpr double pi = 3.14159265358979323846;
+};
+
+} // namespace CloudJ
